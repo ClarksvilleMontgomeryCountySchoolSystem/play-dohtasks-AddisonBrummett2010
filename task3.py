b44901f58fde55f1c5d9adf70f180a748d41b6e0
AddisonBrummett2010 <addison.brummett531@students.cmcss.net>
2026-01-16 10:02:25 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 def main():
     color1 = "gray"
     color2 = "pink"
-    print(f"1) Roll a ball using {color1}")
+    print(f"1) Roll a ball using {color1}.")
     choice1 = input("long body or round body? ")
     if choice1 == "long body":
         print("2) Roll the ball into an egg shape.")
@@ -11,7 +11,7 @@ def main():
     print("4) Attach the head to one end of the body.")
     choice2 = input("long tail or short tail? ")
     if choice2 == "long tail":
-        print(f"5) Roll a thing rope using {color2} to the back.")
+        print(f"5) Roll a thin rope using {color2} and attach to the back.")
     else:
         print(f"5) Add a small bump using {color2} to the back.")
     print(f"6) Add four small legs to the bottom using {color1}.")
